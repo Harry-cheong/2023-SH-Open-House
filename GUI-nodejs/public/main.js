72b6871d03055bb34a1991b5a3b2ec6bac64dabb
sawzedong <saw.zedong@dhs.sg>
2023-01-10 22:49:17 +0800

    Added basic functionality to GUI Added function to designate code blocks and add code blocks Added function to pull a string representing code blocks

--- GUI-nodejs/public/main.js
+++ GUI-nodejs/public/main.js
@@ -7,8 +7,25 @@ function clearAllCode() {
     console.log("all code cleared")
 }
 
+function pullCodeSequenceToString() {
+    finalText = ""
+    var allBlocks = $("#codecontainer .block")
+    for(var i = 0; i < allBlocks.length; i++) {
+        var id = allBlocks[i].id.split('_')[1];
+        console.log(allBlocks[i].id)
+        var val = $(`#${allBlocks[i].id} input`).val()
+        finalText = finalText + id + val;
+        if (i !== allBlocks.length-1) {
+            finalText = finalText + "-";
+        }
+    }
+    console.log(finalText)
+    $("#name").val(finalText)
+}
+
 function runCode() {
-    $("#runmodal").modal("show");
+    pullCodeSequenceToString();
+    // $("#runmodal").modal("show");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -17,7 +34,7 @@ function runCode() {
     };
     xhttp.open("POST", "/formdata", true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(`cmd=${$("#name").val()}`);    
+    // xhttp.send(`cmd=${$("#name").val()}`);    
 }
 
 function stopCode() {
@@ -71,6 +88,50 @@ function reloadToCompletion() {
     xhttp.send();    
 }
 
+// GUI rendering code blocks
+let isOdd = true;
+let codeCounter = 0;
+
+function addToCode(key) {
+    contents = $(`#${key}`).html();
+    copy = $(`<button id="${codeCounter}_${key}" class="block"></button>`);
+    $('#codecontainer').append(copy.append(contents));
+    $(`#${codeCounter}_${key} input`).prop('disabled', false);
+    $(`#${codeCounter}_${key} input`).on('input', function(e) {
+        value = e.target.value
+    });
+    codeCounter += 1;
+}
+
+const allBlocks = {
+    "fwd": {pretext: "Move forward ", posttext: "°", inputRequired: true, inputType: "number", max: 10000, min: 1},
+    "right": {pretext: "Turn right ", posttext: "°", inputRequired: true, inputType: "number", max: 10000, min: 1},
+    "left": {pretext: "Turn left ", posttext: "°", inputRequired: true, inputType: "number", max: 10000, min: 1},
+    "bwd": {pretext: "Move backward ", posttext: "°", inputRequired: true, inputType: "number", max: 10000, min: 1},
+}
+
+
 $(function() {
     reloadToCompletion();
+    for (const [key, value] of Object.entries(allBlocks)) {
+        contents = $('#template').html();
+        copy = $(`<button id="${key}" class="block" onclick="addToCode('${key}')"></button>`);
+        if (isOdd) {
+            $('#allblocksleft').append(copy.append(contents));
+        } else {
+            $('#allblocksright').append(copy.append(contents));
+        }
+        isOdd = !isOdd;
+        $(`#${key} .preblockname`).text(value.pretext);
+        $(`#${key} .postblockname`).text(value.posttext);
+        if (value.inputRequired) {
+            $(`#${key} .blockdetail`).attr('type', value.inputType);
+            $(`#${key} .blockdetail`).attr('min', value.min);
+            $(`#${key} .blockdetail`).attr('max', value.max);
+        } else {
+            $(`#${key} .blockdetail`).addClass('d-none');
+        }
+        
+        console.log(`${key}: ${value}`);
+    }
 })
